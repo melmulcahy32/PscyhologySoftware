7fc45da3b546ab4c5413d1d8822bff42f0702dd8
melmulcahy <melmulcahy32@gmail.com>
2014-11-05 16:31:01 -0500

    Renamed FirstExample to DatabaseExample.

--- src/DatabaseExample.java
+++ src/DatabaseExample.java
@@ -3,7 +3,7 @@
  */
 import java.sql.*;
 
-public class FirstExample {
+public class DatabaseExample {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/Psychology_Software";
