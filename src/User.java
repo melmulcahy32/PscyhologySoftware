649f6f3d1b0d31dd48683be7b062a4499e1872cd
anhar <salih2011@my.ccsu.edu>
2014-11-09 08:04:16 -0500

    Matt's student class has been merged with Anhar's student class.

--- src/User.java
+++ src/User.java
@@ -11,15 +11,34 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.Locale;
+import java.util.Scanner;
 
 abstract class User {
     abstract void viewPassage() throws IOException;
-    //abstract void answerSMS();
-    //abstract void answerQuiz();
-    //abstract void frame();
+    abstract void answerSMS();
+    abstract void answerQuiz();
 }
 
-class Student extends User {
+class Student extends User implements ActionListener {
+    ArrayList timeSMS = new ArrayList();
+    int timePassage;
+    int idNumber;
+    boolean finishedReading = false;
+    boolean answeredQuestion = false;
+    boolean answeredSMS = false;
+
+    JFrame frame = new JFrame("answerQuiz");
+    JFrame frame1 = new JFrame("answerSMS");
+    JFrame frame2 = new JFrame("viewPassage");
+
+    JPanel panel = new JPanel();
+    JPanel panel1 = new JPanel();
+
+    JRadioButton j = new JRadioButton();
+
+    JButton button = new JButton("Submit");
+    JButton button1 = new JButton("Read");
+
     @Override
     public void viewPassage() throws IOException {
         JFrame frame = new JFrame("Test");
@@ -32,6 +51,35 @@ class Student extends User {
         JButton button = new JButton("Read Passage");
         panel.add(button);
         button.addActionListener (new Action1());
+    }
+
+    public void answerSMS()
+    {
+        frame1.setLocationRelativeTo(null);
+        frame1.pack();
+        frame1.setSize(500,500);
+        String answer = JOptionPane.showInputDialog("Text here ");
+        Scanner scan = new Scanner(answer);
+        answer = scan.nextLine();
+        frame1.setVisible(true);
+    }
+
+    public void answerQuiz()
+    {
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setSize(500,500);
+        frame.add(panel);
+        panel.add(j);
+        button.setSize(20,20);
+        panel.add(button);
+        button1.addActionListener(this);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        JOptionPane.showMessageDialog(frame,"Changs Submitted!","Confirmation",JOptionPane.PLAIN_MESSAGE);
     }
 }
 
@@ -73,7 +121,6 @@ class Action2 implements ActionListener{
 
         JButton button3 = new JButton("Submit");
         panel2.add(button3);
-        //this.dispose();
     }
 }
 
@@ -81,5 +128,7 @@ class Runner{
     public static void main(String[] args) throws IOException {
         Student s = new Student();
         s.viewPassage();
+        s.answerSMS();
+        s.answerQuiz();
     }
 }
